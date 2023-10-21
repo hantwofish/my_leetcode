de3840266f26dbff1ad949ccf89d81eeadf8be14
hkk <961373042@qq.com>
2023-10-21 23:14:34 +0800

    stash

---
+++ rm_exe.sh
@@ -0,0 +1,4 @@
+# rm -rf *.exe
+
+find . -name "*.exe" -exec rm {} \;
+find . -name "tempCodeRunnerFile.cpp" -exec rm {} \;
---
+++ upload.sh
@@ -0,0 +1,5 @@
+bash rm_exe.sh
+git status
+git add *.cpp *.txt *.sh
+git commit -m "stash"
+git push origin personal
