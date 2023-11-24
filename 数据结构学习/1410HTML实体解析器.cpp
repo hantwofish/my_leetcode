90cb75b0dfa920877ef6dd227b623e35a69c3b09
hkk <961373042@qq.com>
2023-11-24 21:18:46 +0800

    1410 html实体解析器

--- 数据结构学习/1410HTML实体解析器.cpp
+++ 数据结构学习/1410HTML实体解析器.cpp
@@ -29,27 +29,50 @@ public:
         string resu = "";
         while ( i < len) {
             if (text[i] == '&') {
-                j = i + 1;
-                while (j < len && text[j] != ';') {
-                    j++;
+                // 判断后面 4 5 67 个字符是不是 要替换的，是则替换，不是则保持
+                string str4= "";
+                string str5 = str4;
+                string str6 = str4;
+                string str7 = str4 ;
+                if(i + 4  -1< len){
+                    str4 = text.substr(i, 4);
                 }
-                string temp = "";
-                for (int k = i; k <= j; k++) {
-                    temp += text[k];
+                if(i + 5  -1< len){
+                    str5 = text.substr(i, 5);
                 }
-                if(mp.find(temp) ==  mp.end() ){
-                    resu += temp;
-                }else {
-                    resu += mp[temp];
+                if(i + 6  -1< len){
+                    str6 = text.substr(i, 6);
                 }
+                if(i + 7  -1< len){
+                    str7 = text.substr(i, 7);
+                }
+                cout <<"i= " << i <<  " " << str4 << " |  " << str5 << " | " << str6 << " | " << str7 << endl;
 
-                // cout << "temp= " << temp << " resu= " << resu << endl;
+                if( mp.find(str4) != mp.end()){
+                    resu += mp[str4];
+                    i+=4;
 
-                i = j + 1;
+                }else if( mp.find(str5) != mp.end()){
+                    resu += mp[str5];
+                    i+=5;
+
+                }else if( mp.find(str6) != mp.end()){
+                    resu += mp[str6];
+                    i+=6;
+                
+                }else if( mp.find(str7) != mp.end()){
+                    resu += mp[str7];
+                    i+=7;
+                }else{
+                    resu += text[i];
+                    i++;
+                }
+                
             } else {
                 resu += text[i];
                 i++;
             }
+          
             cout << "resu= " << resu << endl;
         }
 
@@ -74,12 +97,14 @@ public:
 int main()
 {
     Solution s1;
-    string str = "&&gt";
+    string str = "1&&gt;";
     string resu = s1.entityParser(str);
     cout << "2sresu=  " << resu << endl;
 
     return 0;
 }   
+
+
 
 
 
