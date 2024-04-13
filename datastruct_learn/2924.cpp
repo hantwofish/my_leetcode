cbd6c13b787815f76f74ee43f7ebf86374e1dd9a
hkk <961373042@qq.com>
2024-04-13 10:17:46 +0800

    图：从一个节点访问所有节点

--- datastruct_learn/2924.cpp
+++ datastruct_learn/2924.cpp
@@ -4,79 +4,7 @@
 
 using namespace std;
 
-class Solution2 {
-public:
-    int findChampion(int n, vector<vector<int>>& edges) {
-        N = n;
-        for(int i = 0; i< edges.size(); i++){
-            figureMap[edges[i][0]].push_back(edges[i][1]);
-        }
-        // PrintMap();
-        for(int i = 0 ; i< N; i++){
-            canVisited.clear();
-            canVisited.resize(N,0);
-            DFS(i,i);
-
-           if(isAllFind(canVisited)){
-                return i;
-           }
-
-        }
-
-        return -1;
-    }
-    bool isAllFind(vector<int>&canVisited)
-    {
-
-        int i = 0;
-        while(i < canVisited.size()) {
-            if(canVisited[i] == 0){
-                return false;
-            }
-            i++;
-        }
-        return true;
-    }
-    void DFS(int start, int cur_index)
-    {
-        canVisited[cur_index] = 1;
-        if(cur_index >= N) return;
-        // if(visited[cur_index]) return;
-        // cout << "  cur= " << cur_index << endl;
-    
-        // resu.push_back(cur_index);
-        // visited[cur_index] = true;
-
-        // MyPrintOne(resu);
-
-        // vector<int>neigh = figureMap[cur_index];
-        for(int i = 0; i < figureMap[cur_index].size(); i++){
-
-            DFS(start, figureMap[cur_index][i]);
-
-        }
-        // resu.pop_back();
-        // visited[cur_index] = false;
-
-    }
-    void PrintMap()
-    {
-        auto it = figureMap.begin();
-        while(it != figureMap.end()){
-            cout << it->first << " | ";
-            for(auto i : it->second){
-                cout << i << " ";
-            }
-            cout << endl;
-            it++;
-        }
-    }
-private:
-    unordered_map<int,vector<int>>figureMap;
-    int N;
-    vector<int>canVisited;
-}; 
-
+// 从某个节点访问所有节点, 回溯
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
@@ -91,38 +19,26 @@ public:
             canVisited.clear();
             canVisited.resize(N,0);
             DFS(i,i, resu, visited);
-
-           if(isAllFind(canVisited)){
-                return i;
-           }
+            
+            cout << "i= " << i << endl;
+           MyPrintOne(canVisited);
 
         }
 
         return -1;
     }
-    bool isAllFind(vector<int>&canVisited)
-    {
 
-        int i = 0;
-        while(i < canVisited.size()) {
-            if(canVisited[i] == 0){
-                return false;
-            }
-            i++;
-        }
-        return true;
-    }
     void DFS(int start, int cur_index, vector<int>&resu, vector<int>&visited)
     {
         canVisited[cur_index] = 1;
         if(cur_index >= N) return;
         if(visited[cur_index]) return;
-        cout << "  cur= " << cur_index << endl;
+        // cout << "  cur= " << cur_index << endl;
     
         resu.push_back(cur_index);
         visited[cur_index] = true;
 
-        MyPrintOne(resu);
+        // MyPrintOne(resu);
 
         vector<int>neigh = figureMap[cur_index];
         for(int i = 0; i < neigh.size(); i++){
@@ -137,7 +53,7 @@ public:
     void PrintMap()
     {
         auto it = figureMap.begin();
-        while(it != figureMap.end()){
+        while(it != figureMap.end()) {
             cout << it->first << " | ";
             for(auto i : it->second){
                 cout << i << " ";
@@ -153,46 +69,13 @@ private:
 }; 
 
 
-// 计算入度 ,只有一个 入度为 0 的节点
-class Solution3 {
-public:
-    int findChampion(int n, vector<vector<int>>& edges) {
-        N = n;
-        for(int i = 0; i < N; i++){
-            indig[i] = 0;
-        }
-        for(int i = 0; i< edges.size(); i++){
-            figureMap[edges[i][0]].push_back(edges[i][1]);
-            indig[edges[i][1]]++;
-        }
 
-        int zeroindig_num = 0;
-        int zero_indig_index  = -1;
-        auto it = indig.begin();
-        while(it != indig.end()){
-            if(it->second == 0){
-                zero_indig_index =  it->first;
-            }
-            it++;
-        }
-        if(zeroindig_num != 1){
-            return -1;
-        }
-        return zero_indig_index;
-    }
-private:
-    unordered_map<int,vector<int>>figureMap;
-    int N;
-    vector<int>canVisited;
-    unordered_map<int,int>indig;
-};
 int main()
 {
-    int n = 4;
-    vector<vector<int>>edges = {{0,2},{1,2},{1,3}};
+    int n = 6;
+    vector<vector<int>>edges = {{0,1},{0,4},{1,2},{1,3},{5,0},{5,4}};
     Solution S1;
     int resu= S1.findChampion(n, edges);
     
-    cout << "resu= " << resu  << endl;
     return 0;
 }
