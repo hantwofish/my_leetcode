a2cf271d6c7460161bd87782a424eca6ee72392c
hkk <961373042@qq.com>
2024-04-18 07:59:30 +0800

    feature(leetcode): 2589
    algorithm: not_solve
    
    description: 2589. 完成所有任务的最少时间

---
+++ datastruct_learn/2589.cpp
@@ -0,0 +1,19 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+
+
+using namespace std;
+
+class Solution {
+public:
+    int findMinimumTime(vector<vector<int>>& tasks) {
+        
+    }
+}; 
+
+int main()
+{
+    
+    cout << "hell22o" << endl;
+    return 0;
+}
