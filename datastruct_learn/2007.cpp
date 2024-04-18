25e22686d2c561568afbf55ba445f71898d42b6e
hkk <961373042@qq.com>
2024-04-18 12:40:50 +0800

    2007

--- datastruct_learn/2007.cpp
+++ datastruct_learn/2007.cpp
@@ -3,7 +3,7 @@
 
 
 using namespace std;
-class Solution {
+class Solution2 {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
         if(changed.size() % 2 ==1) return {};
@@ -48,13 +48,39 @@ private:
     
 };
  
-
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        if(changed.size() % 2 == 1) return {};
+        vector<int>resu;
+        sort(changed.begin(),changed.end());
+        for(auto i : changed){
+            count_times[i]++;
+        }
+        for(int cur : changed){
+            if(count_times[cur] == 0) continue;
+            // 
+            if(count_times[cur * 2] == 0) return {};
+            resu.push_back(cur);
+            count_times[cur]--;
+            count_times[cur * 2]--;
+        }
+        return resu;
+    }
+private:
+    unordered_map<int,int>count_times;
+};
 int main()
 {
     Solution s1;
     vector<int> changed = {1,3,4,2,6,8};
-    s1.findOriginalArray(changed);
-
+    // s1.findOriginalArray(changed);
+    unordered_map<int,int>count_times;
+    int resu = count_times[1]; // 不存在的时候,新建一个,会导致容量+1
+    cout << "resu= " <<resu << endl;
+    cout << "size= " << count_times.size() << endl;
+    cout << "resu= " <<  count_times[0] << endl;
+    cout << "size= " << count_times.size() << endl;
     // multiset<int>meset;
     // meset.insert(1); meset.insert(1); meset.insert(12);
     // cout << "mesetsize= " << meset.size() << endl;
