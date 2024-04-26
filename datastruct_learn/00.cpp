3a3be80a1934a7ff4ed450f03f8c3439385f0b29
hkk <961373042@qq.com>
2024-04-26 19:12:33 +0800

    xx

--- datastruct_learn/00.cpp
+++ datastruct_learn/00.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 int main()
 {
+    long long num = 0;
+    long long minNum =  -1 * ( (long long)1 << 32);
+    long long maxNum = (long long)1 << 32;
+    cout << minNum << endl;
+    cout << maxNum << endl;
     
     cout << "hell22o" << endl;
     return 0;
