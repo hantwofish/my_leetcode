92b9a0f4364a4869093a8f4d0795867fe29b4f23
hkk <961373042@qq.com>
2024-04-26 19:12:07 +0800

    feature(leetcode): 2385
    algorithm: not_solve
    
    description: 2385. 感染二叉树需要的总时间

---
+++ datastruct_learn/2385.cpp
@@ -0,0 +1,20 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+#include"../TREE.h"
+
+
+using namespace std;
+class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        return 0;
+    }
+};
+ 
+
+int main()
+{
+    
+    cout << "hell22o" << endl;
+    return 0;
+}
