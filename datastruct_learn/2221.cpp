247a83eb2d00f2dcf15443061fa8dc4b5c3e1dda
hkk <961373042@qq.com>
2024-05-14 08:27:38 +0800

    leetcode 2221

---
+++ datastruct_learn/2221.cpp
@@ -0,0 +1,58 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+#define TIMEINTERVAL 1
+
+using namespace std;
+
+void timer_function(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    // std::cout << "Timer expired after " << seconds << " seconds" << std::endl;
+    
+    cout << endl;
+    cout << "[error] timer " <<  seconds << " is over exit " << endl;
+    exit(1);
+}
+
+
+
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int N = nums.size();
+        for(int i= 0; i< N; i++){ // 次数
+            for(int j = 0; j < N -i -1; j++){
+                nums[j] = (nums[j] + nums[j+1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+};
+
+int mainFunc()
+{
+    Solution s1;
+    vector<int>nums = {1,2,3,4,5};
+    s1.triangularSum(nums);
+
+    return 0;
+}
+
+int main()
+{
+    std::cout << "[info] Starting timer for " << TIMEINTERVAL << " seconds" << std::endl << std::endl;
+    std::thread timer_thread(timer_function, TIMEINTERVAL); // 设置计时器延时 TIMEINTERVAL 秒
+
+    if(mainFunc() == 0){
+        // timer_thread.join(); // 等待计时器线程完成
+        cout << "[info] main end" << endl << endl;
+        return 0;
+    }
+
+    timer_thread.join(); // 等待计时器线程完成
+    std::cout << "[info] Timer thread finished" << std::endl;
+    return 0;
+}
