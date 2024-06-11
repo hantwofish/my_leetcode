6e5876c626e6975d044bb95aea6e3f6f6610aa72
hkk <961373042@qq.com>
2024-06-12 07:55:09 +0800

    2589

--- datastruct_learn/2589.cpp
+++ datastruct_learn/2589.cpp
@@ -1,19 +1,53 @@
 #include"../STLCOMMON.h"
 #include"../COMMONALO.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
 
+#define TIMEINTERVAL 1
 
 using namespace std;
+
+void timer_function(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    // std::cout << "Timer expired after " << seconds << " seconds" << std::endl;
+    
+    cout << endl;
+    cout << "[error] timer " <<  seconds << " is over exit " << endl;
+    exit(1);
+}
+
+
 
 class Solution {
 public:
     int findMinimumTime(vector<vector<int>>& tasks) {
         
+        return 0;
     }
-}; 
+};
+
+int mainFunc()
+{
+    Solution s1;
+    vector<vector<int>> tasks = {{2,3,1},{4,5,1},{1,5,2}};
+    int resu = s1.findMinimumTime(tasks);
+    cout << "resu = " << resu << endl;
+    return 0;
+}
 
 int main()
 {
-    
-    cout << "hell22o" << endl;
+    std::cout << "[info] Starting timer for " << TIMEINTERVAL << " seconds" << std::endl << std::endl;
+    std::thread timer_thread(timer_function, TIMEINTERVAL); // 设置计时器延时 TIMEINTERVAL 秒
+
+    if(mainFunc() == 0){
+        // timer_thread.join(); // 等待计时器线程完成
+        cout << "[info] main end" << endl << endl;
+        return 0;
+    }
+
+    timer_thread.join(); // 等待计时器线程完成
+    std::cout << "[info] Timer thread finished" << std::endl;
     return 0;
 }
