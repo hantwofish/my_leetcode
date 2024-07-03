8512161dd58e7180492b7fa639f0e1590863f038
hkk <961373042@qq.com>
2024-07-03 08:18:59 +0800

    3099

---
+++ datastruct_learn/3099.cpp
@@ -0,0 +1,62 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "../TREE.h"
+
+#define TIMEINTERVAL 1
+
+using namespace std;
+
+void timer_function(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+    // std::cout << "Timer expired after " << seconds << " seconds" << std::endl;
+    
+    cout << endl;
+    cout << "[error] timer " <<  seconds << " is over exit " << endl;
+    exit(1);
+}
+
+
+
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int val = x;
+        while(val > 0){
+            cout << val % 10 << endl;
+            sum += (val % 10);
+            val = val / 10;
+        }
+        if(x % sum == 0){
+            return sum;
+        }
+        return -1;
+    }
+};
+
+int mainFunc()
+{
+    Solution s1;
+    s1.sumOfTheDigitsOfHarshadNumber(198);
+
+    return 0;
+}
+
+int main()
+{
+    std::cout << "[info] Starting timer for " << TIMEINTERVAL << " seconds" << std::endl << std::endl;
+    std::thread timer_thread(timer_function, TIMEINTERVAL); // 设置计时器延时 TIMEINTERVAL 秒
+
+    if(mainFunc() == 0){
+        // timer_thread.join(); // 等待计时器线程完成
+        cout << "[info] main end" << endl << endl;
+        return 0;
+    }
+
+    timer_thread.join(); // 等待计时器线程完成
+    std::cout << "[info] Timer thread finished" << std::endl;
+    return 0;
+}
