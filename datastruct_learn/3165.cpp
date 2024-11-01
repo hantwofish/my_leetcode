b3e9c27f45dedd9111065783408497aef56dc5df
hkk <961373042@qq.com>
2024-11-01 19:14:03 +0800

    3165

---
+++ datastruct_learn/3165.cpp
@@ -0,0 +1,49 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "../TREE.h"
+
+#define TIMEINTERVAL 1
+
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    int maximumSumSubsequence(vector<int>& nums, vector<vector<int>>& queries) {
+
+      return 0;  
+    }
+    int calMaxSum(vector<int>&nums)
+    {
+
+        
+    }
+};
+
+int mainFunc()
+{
+    Solution s1;
+
+    cout << "end" << endl;
+    return 0;
+}
+
+int main()
+{
+
+    if(mainFunc() == 0){
+        // timer_thread.join(); // 等待计时器线程完成
+        cout << "[info] main end" << endl << endl;
+        return 0;
+    }
+
+    return 0;
+}
+
+
+// g++ 2766.cpp -std=c++11 -o main.exe  && ./main.exe
