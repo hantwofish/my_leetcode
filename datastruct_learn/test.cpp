e3007d39b0ff86feafbfb54534dbf2fb24fa37a4
hkk <961373042@qq.com>
2024-11-14 08:31:59 +0800

    test

---
+++ datastruct_learn/test.cpp
@@ -0,0 +1,20 @@
+#include"../STLCOMMON.h"
+#include"../COMMONALO.h"
+#include <iostream>
+#include "../TREE.h"
+
+
+using namespace std;
+
+
+
+int main()
+{
+
+   cout << "[info] main end" << endl << endl;
+
+    return 0;
+}
+
+
+// g++ 2766.cpp -std=c++11 -o main.exe  && ./main.exe
