f2e65fd747e96acf598cde996836be11016f8830
hantwofish <hankangkang5@huawei.com>
2024-12-07 16:46:07 +0800

    delete file

--- datastruct_learn/run.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-filename=$1
-# g++ --std=c++11 ./datastruct_learn/${filename} -o ./test.exe
-# ./test.exe
-g++ --std=c++11 ${filename} -o main.exe && ./main.exe
-
-cls;  g++ --std=c++11 2187.cpp -o main.exe ; .\main.exe
